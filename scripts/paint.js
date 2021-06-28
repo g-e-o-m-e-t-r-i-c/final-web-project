33a567d8e6487d1290c954dad4998220c3db11dd
g-e-o-m-e-t-r-i-c <darrenyap08@gmail.com>
2021-06-28 15:23:14 +0800

    added scripts directory

---
+++ scripts/paint.js
@@ -0,0 +1,42 @@
+var rect = document.getElementById("rectangle");
+var c = document.getElementById("canv");
+var ctx = c.getContext("2d");
+console.log("Canvas Width: " + c.width + " | Canvas Height: " + c.height);
+document.getElementById("intro").innerHTML +=
+    "Canvas Width: " + c.width + " | Canvas Height: " + c.height;
+
+var submitRect = document.getElementById("submitRect");
+submitRect.addEventListener("click", makeRect);
+
+function makeRect() {
+    // set up variables based on the user input in the text fields
+    var x1 = Number(document.getElementById("x1").value);
+    var y1 = Number(document.getElementById("y1").value);
+    var width = Number(document.getElementById("x2").value);
+    var height = Number(document.getElementById("y2").value);
+    var colorVal = document.getElementById("colors").value;
+
+    console.log(x1, y1, width, height, colorVal); // debugging
+
+    // since JS automates the process if user input is out of range, we don't need to settle it
+    /* eg. if x-coord/y-coord is negative, it is automatically set to 0
+	if width/height is out of bourds, it is set to the maximum value 
+	if color is not in hexadecimal, it will be set to #000000
+	*/
+    if (x1 > c.width) {
+        x1 = 0;
+        console.log("Out of bounds: Setting X-Coordinate to 0");
+    }
+    if (y1 > c.height) {
+        y1 = 0;
+        console.log("Out of bounds: Setting Y-Coordinate to 0");
+    }
+
+    ctx.fillStyle = "#" + colorVal;
+    ctx.fillRect(x1, y1, width, height);
+    document.getElementById("x1").value = "";
+    document.getElementById("y1").value = "";
+    document.getElementById("x2").value = "";
+    document.getElementById("y2").value = "";
+    document.getElementById("colors").value = "";
+}
