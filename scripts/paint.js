59474f55d29231030dc773491db7a8e46a4e1495
g-e-o-m-e-t-r-i-c <darrenyap08@gmail.com>
2021-06-28 17:06:05 +0800

    updated comments

--- scripts/paint.js
+++ scripts/paint.js
@@ -21,8 +21,8 @@ function makeRect() {
     // since JS automates the process if user input is out of range, we don't need to settle it
     /* eg. if x-coord/y-coord is negative, it is automatically set to 0
 	if width/height is out of bourds, it is set to the maximum value 
-	if color is not in hexadecimal, it will be set to previous colour used
-	*/
+	if color is not in hexadecimal range, it will be set to previous colour used
+*/
     if (x1 > c.width) {
         x1 = 0;
         console.log("Out of bounds: Setting X-Coordinate to 0");
