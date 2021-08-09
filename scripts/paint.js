5aa9e2afbb08bd35f43c98fa2326887001986acf
g-e-o-m-e-t-r-i-c <darrenyap08@gmail.com>
2021-08-09 08:29:38 +0800

    fixed indentation and spacing

--- scripts/paint.js
+++ scripts/paint.js
@@ -17,21 +17,16 @@ function makeRect() {
 	var height = Number(document.getElementById("y2").value);
 	var colorVal = document.getElementById("colors").value;
 
-	console.log(x1, y1, width, height, colorVal); // debugging
-
-	// since JS automates the process if user input is out of range, we don't need to settle it
-	/* eg. if x-coord/y-coord is negative, it is automatically set to 0
-	if width/height is out of bourds, it is set to the maximum value 
-	if color is not in hexadecimal range, it will be set to previous colour used
-    */
 	if (x1 > c.width) {
 		x1 = 0;
 		console.log("Out of bounds: Setting X-Coordinate to 0");
 	}
+
 	if (y1 > c.height) {
 		y1 = 0;
 		console.log("Out of bounds: Setting Y-Coordinate to 0");
 	}
+
 	// driver code
 	ctx.fillStyle = "#" + colorVal;
 	ctx.fillRect(x1, y1, width, height);
