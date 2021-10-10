c492e26b3e692cacda066632552281988909e0ef
g-e-o-m-e-t-r-i-c <darrenyap08@gmail.com>
2021-10-10 09:42:58 +0800

    formatting overhaul for less data transfer

--- scripts/paint.js
+++ scripts/paint.js
@@ -1,45 +1,45 @@
 // canvas set-up
-var rect = document.getElementById("rectangle");
-var c = document.getElementById("canv");
-var ctx = c.getContext("2d");
+var rect = document.getElementById('rectangle')
+var c = document.getElementById('canv')
+var ctx = c.getContext('2d')
 
-document.getElementById("intro").innerHTML +=
-	"canvas width: <b>" +
+document.getElementById('intro').innerHTML +=
+	'canvas width: <b>' +
 	c.width +
-	"</b> | canvas height: <b>" +
+	'</b> | canvas height: <b>' +
 	c.height +
-	"</b>"; // let user know canvas dimensions
+	'</b>' // let user know canvas dimensions
 
-var submitRect = document.getElementById("submitRect");
-submitRect.addEventListener("click", makeRect);
+var submitRect = document.getElementById('submitRect')
+submitRect.addEventListener('click', makeRect)
 
 function makeRect() {
 	// obtain values from user input in input fields
-	var x1 = Number(document.getElementById("x1").value);
-	var y1 = Number(document.getElementById("y1").value);
-	var width = Number(document.getElementById("x2").value);
-	var height = Number(document.getElementById("y2").value);
-	var colorVal = document.getElementById("colors").value;
+	var x1 = Number(document.getElementById('x1').value)
+	var y1 = Number(document.getElementById('y1').value)
+	var width = Number(document.getElementById('x2').value)
+	var height = Number(document.getElementById('y2').value)
+	var colorVal = document.getElementById('colors').value
 
 	// notify user when coordinates entered out of bounds
 	if (x1 > c.width) {
-		x1 = 0;
-		console.log("out of bounds: setting X-coordinate to 0");
+		x1 = 0
+		console.log('out of bounds: setting X-coordinate to 0')
 	}
 
 	if (y1 > c.height) {
-		y1 = 0;
-		console.log("out of bounds: setting Y-coordinate to 0");
+		y1 = 0
+		console.log('out of bounds: setting Y-coordinate to 0')
 	}
 
 	// driver code - draw rectangle and set fill color, according to the user's input
-	ctx.fillStyle = "#" + colorVal;
-	ctx.fillRect(x1, y1, width, height);
+	ctx.fillStyle = '#' + colorVal
+	ctx.fillRect(x1, y1, width, height)
 
 	// reset fields after rectangle is drawn
-	document.getElementById("x1").value = "";
-	document.getElementById("y1").value = "";
-	document.getElementById("x2").value = "";
-	document.getElementById("y2").value = "";
-	document.getElementById("colors").value = "";
+	document.getElementById('x1').value = ''
+	document.getElementById('y1').value = ''
+	document.getElementById('x2').value = ''
+	document.getElementById('y2').value = ''
+	document.getElementById('colors').value = ''
 }
