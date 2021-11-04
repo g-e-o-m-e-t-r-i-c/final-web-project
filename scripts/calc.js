92e5f64572130c2583e1311e7f3b83e2d5afc0d0
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2021-11-04 15:46:49 +0800

    changed 2 space indentation to size 2 tabs

--- scripts/calc.js
+++ scripts/calc.js
@@ -2,111 +2,111 @@
 document.getElementById('submit').addEventListener('click', calculateIt)
 
 function calculateIt() {
-  var myOperator // operator selected by user
-  var returnValue // output of the operation
+	var myOperator // operator selected by user
+	var returnValue // output of the operation
 
-  // obtain the operands
-  var operand1 = document.getElementById('operand1').value
-  var operand2 = document.getElementById('operand2').value
+	// obtain the operands
+	var operand1 = document.getElementById('operand1').value
+	var operand2 = document.getElementById('operand2').value
 
-  // obtain the operand types (string/number)
-  var operand1type = document.getElementById('operand1-type').value
-  var operand2type = document.getElementById('operand2-type').value
+	// obtain the operand types (string/number)
+	var operand1type = document.getElementById('operand1-type').value
+	var operand2type = document.getElementById('operand2-type').value
 
-  // obtain the operator
-  var radios = document.getElementsByName('operator')
+	// obtain the operator
+	var radios = document.getElementsByName('operator')
 
-  // convert the operands to the datatype specified
-  switch (operand1type) {
-    case 'string':
-      operand1 = String(operand1)
-      break
-    case 'number':
-      operand1 = Number(operand1)
-      break
-  }
+	// convert the operands to the datatype specified
+	switch (operand1type) {
+		case 'string':
+			operand1 = String(operand1)
+			break
+		case 'number':
+			operand1 = Number(operand1)
+			break
+	}
 
-  switch (operand2type) {
-    case 'string':
-      operand2 = String(operand2)
-      break
-    case 'number':
-      operand2 = Number(operand2)
-      break
-  }
+	switch (operand2type) {
+		case 'string':
+			operand2 = String(operand2)
+			break
+		case 'number':
+			operand2 = Number(operand2)
+			break
+	}
 
-  // loop through each possible operand value and find the selected one
-  for (var i = 0, length = radios.length; i < length; i++) {
-    if (radios[i].checked) {
-      myOperator = radios[i].value
+	// loop through each possible operand value and find the selected one
+	for (var i = 0, length = radios.length; i < length; i++) {
+		if (radios[i].checked) {
+			myOperator = radios[i].value
 
-      // execute a different operation depending on which operator was selected
-      switch (radios[i].value) {
-        // -- Arithmetic --
-        case '+':
-          returnValue = operand1 + operand2
-          break
-        case '-':
-          returnValue = operand1 - operand2
-          break
-        case '*':
-          returnValue = operand1 * operand2
-          break
-        case '/':
-          returnValue = operand1 / operand2
-          break
-        case '%':
-          returnValue = operand1 % operand2
-          break
+			// execute a different operation depending on which operator was selected
+			switch (radios[i].value) {
+				// -- Arithmetic --
+				case '+':
+					returnValue = operand1 + operand2
+					break
+				case '-':
+					returnValue = operand1 - operand2
+					break
+				case '*':
+					returnValue = operand1 * operand2
+					break
+				case '/':
+					returnValue = operand1 / operand2
+					break
+				case '%':
+					returnValue = operand1 % operand2
+					break
 
-        // -- String --
-        case 'concat':
-          returnValue = operand1 + operand2
-          myOperator = '+'
-          break
+				// -- String --
+				case 'concat':
+					returnValue = operand1 + operand2
+					myOperator = '+'
+					break
 
-        // -- Logic --
-        case '==':
-          returnValue = operand1 == operand2
-          break
-        case '===':
-          returnValue = operand1 === operand2
-          break
-        case '!=':
-          returnValue = operand1 != operand2
-          break
-        case '!==':
-          returnValue = operand1 !== operand2
-          break
-        case '>':
-          returnValue = operand1 > operand2
-          break
-        case '>=':
-          returnValue = operand1 >= operand2
-          break
-        case '<':
-          returnValue = operand1 < operand2
-          break
-        case '<=':
-          returnValue = operand1 <= operand2
-          break
-      }
+				// -- Logic --
+				case '==':
+					returnValue = operand1 == operand2
+					break
+				case '===':
+					returnValue = operand1 === operand2
+					break
+				case '!=':
+					returnValue = operand1 != operand2
+					break
+				case '!==':
+					returnValue = operand1 !== operand2
+					break
+				case '>':
+					returnValue = operand1 > operand2
+					break
+				case '>=':
+					returnValue = operand1 >= operand2
+					break
+				case '<':
+					returnValue = operand1 < operand2
+					break
+				case '<=':
+					returnValue = operand1 <= operand2
+					break
+			}
 
-      break
-    }
-  }
+			break
+		}
+	}
 
-  // add "" to string operands
-  if (typeof operand1 === 'string') {
-    operand1 = '"' + operand1 + '"'
-  }
-  if (typeof operand2 === 'string') {
-    operand2 = '"' + operand2 + '"'
-  }
+	// add "" to string operands
+	if (typeof operand1 === 'string') {
+		operand1 = '"' + operand1 + '"'
+	}
+	if (typeof operand2 === 'string') {
+		operand2 = '"' + operand2 + '"'
+	}
 
-  // display operation and return value
-  document.getElementById('final-operation').innerHTML =
-    operand1 + ' ' + myOperator + ' ' + operand2
+	// display operation and return value
+	document.getElementById('final-operation').innerHTML =
+		operand1 + ' ' + myOperator + ' ' + operand2
 
-  document.getElementById('result').innerHTML = returnValue
+	document.getElementById('result').innerHTML = returnValue
 }
--- scripts/paint.js
+++ scripts/paint.js
@@ -4,42 +4,42 @@ var c = document.getElementById('canv')
 var ctx = c.getContext('2d')
 
 document.getElementById('intro').innerHTML +=
-  'canvas width: <b>' +
-  c.width +
-  '</b> | canvas height: <b>' +
-  c.height +
-  '</b>' // let user know canvas dimensions
+	'canvas width: <b>' +
+	c.width +
+	'</b> | canvas height: <b>' +
+	c.height +
+	'</b>' // let user know canvas dimensions
 
 var submitRect = document.getElementById('submitRect')
 submitRect.addEventListener('click', makeRect)
 
 function makeRect() {
-  // obtain values from user input in input fields
-  var x1 = Number(document.getElementById('x1').value)
-  var y1 = Number(document.getElementById('y1').value)
-  var width = Number(document.getElementById('x2').value)
-  var height = Number(document.getElementById('y2').value)
-  var colorVal = document.getElementById('colors').value
+	// obtain values from user input in input fields
+	var x1 = Number(document.getElementById('x1').value)
+	var y1 = Number(document.getElementById('y1').value)
+	var width = Number(document.getElementById('x2').value)
+	var height = Number(document.getElementById('y2').value)
+	var colorVal = document.getElementById('colors').value
 
-  // notify user when coordinates entered out of bounds
-  if (x1 > c.width) {
-    x1 = 0
-    console.log('out of bounds: setting X-coordinate to 0')
-  }
+	// notify user when coordinates entered out of bounds
+	if (x1 > c.width) {
+		x1 = 0
+		console.log('out of bounds: setting X-coordinate to 0')
+	}
 
-  if (y1 > c.height) {
-    y1 = 0
-    console.log('out of bounds: setting Y-coordinate to 0')
-  }
+	if (y1 > c.height) {
+		y1 = 0
+		console.log('out of bounds: setting Y-coordinate to 0')
+	}
 
-  // driver code - draw rectangle and set fill color, according to the user's input
-  ctx.fillStyle = '#' + colorVal
-  ctx.fillRect(x1, y1, width, height)
+	// driver code - draw rectangle and set fill color, according to the user's input
+	ctx.fillStyle = '#' + colorVal
+	ctx.fillRect(x1, y1, width, height)
 
-  // reset fields after rectangle is drawn
-  document.getElementById('x1').value = ''
-  document.getElementById('y1').value = ''
-  document.getElementById('x2').value = ''
-  document.getElementById('y2').value = ''
-  document.getElementById('colors').value = ''
+	// reset fields after rectangle is drawn
+	document.getElementById('x1').value = ''
+	document.getElementById('y1').value = ''
+	document.getElementById('x2').value = ''
+	document.getElementById('y2').value = ''
+	document.getElementById('colors').value = ''
 }
